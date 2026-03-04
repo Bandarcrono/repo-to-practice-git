63a752c606e29a014f2be7e535a8a947afd7f41d
Bandarcrono <albertobandarico@gmail.com>
2026-03-03 21:47:31 -0600

    Added Charizard X beacuse it is fun and cool imo.

--- code.py
+++ code.py
@@ -32,3 +32,8 @@ class Pokemon:
 pikachu = Pokemon(25, 'Pikachu', ['Electric'], 'It has small electric sacs on both its cheeks. If threatened, it looses electric charges from the sacs.', True)
 charizard = Pokemon(6, 'Charizard', ['Fire', 'Flying'], 'It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.', False)
 gyarados = Pokemon(130, 'Gyarados', ['Water', 'Flying'], 'It has an extremely aggressive nature. The HYPER BEAM it shoots from its mouth totally incinerates all targets.', False)
+
+mega_charizard_X = Pokemon(6,'Charizard',['Fire','Dragon'],'Coolest Charizard imo',True)
+
+mega_charizard_X.display_details()
+
